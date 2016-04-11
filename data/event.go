79f7105b5fc2fde2012db9bf3273f4c03db52a70
alaypatel07 <alay1431@gmail.com>
2016-04-11 23:47:19 +0530

    Add event.go, has data structures and methods to store events and register them to database

---
+++ data/event.go
@@ -0,0 +1,45 @@
+package data
+
+import (
+	"errors"
+)
+
+var redisClient *RedisDatabaseManager
+
+type Participant struct {
+	Team string `json:"team"`
+	Name string `json:"name"`
+}
+
+type Event struct {
+	Name string `json:"name"`
+	Participants []Participant `json:"participants"`
+}
+
+func (e *Event)Map() (map[string]string, error) {
+	m := make(map[string]string)
+	for _, team := range e.Participants {
+		if m[team.Team] != "" {
+			return nil, errors.New("Same team event twice")
+		}
+		m[team.Team] = team.Name
+	}
+	return m, nil
+}
+
+func init() {
+	redisClient = NewRedisDatabaseManager()
+}
+
+// Register Event to the Database
+func (e *Event)Register() error {
+	if m, err := e.Map(); err != nil {
+		return err
+	} else {
+		if _, err := redisClient.Insert(e.Name, m); err != nil {
+			return err
+		} else {
+			return nil
+		}
+	}
+}
